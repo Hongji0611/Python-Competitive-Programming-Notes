501f2955bbd0888022df283b155ba2220597c019
Jiwoo Hong <hjw6112@gmail.com>
2023-03-15 23:33:07 +0900

    study: JAVA Programmers

---
+++ java/PG_문자열 내 p와 y의 개수.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        s = s.toLowerCase();
+        int p = 0;
+        int y = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == 'p'){
+                p += 1;
+            }else if(s.charAt(i) == 'y'){
+                y += 1;
+            }
+        }
+        if(p == y){
+            answer = true;
+        }else{
+            answer = false;
+        }
+
+        return answer;
+    }
+}
